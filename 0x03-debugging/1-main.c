7fbb9972df7788d75f5f68b2b2a559e17bcbe0e1
mono3gui <mohammedfarahat30@gmail.com>
2023-08-18 06:12:49 -0700

    0x03. C - Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
 * main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
 
 {
@@ -13,7 +14,6 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
 /*while (i < 10)*/
 /*{*/
 /*putchar(i);*/
