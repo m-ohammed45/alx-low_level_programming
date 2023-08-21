faf470f55c0922b4b27597491fcfc7105ec2f38c
mono3gui <mohammedfarahat30@gmail.com>
2023-08-21 12:48:27 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-int pass[100];
+i{nt pass[100];
 int i, sum, n;
 sum = 0;	
 srand(time(NULL));
@@ -18,7 +18,7 @@ for (i = 0; i < 100; i++)
 {
 pass[i] = rand() % 78;
 sum += (pass[i] + '0');
-uutchar(pass[i] + '0');
+putchar(pass[i] + '0');
 if ((2772 - sum) - '0' < 78)
 {
 n = 2772 - sum - '0';
