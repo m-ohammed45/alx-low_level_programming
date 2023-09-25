cb49cd9100c4b7c38179c356e56d9825fcb04547
m-ohammed45 <mohammedfarahat30@gmail.com>
2023-09-25 13:55:56 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print-listint- print a linked list
+ * @h: pointer
+ * Return : number of nodes
+ *
+ */
+size_t print_listint(const listint_t *h);
+{
+      size_t u = 0;
+
+      while (h! = NULL)
+      {
+	      printf("%d\n", h->n);
+              h = h->next;
+	      u++;
+
+      }      
+      return (u);
+ 
+}
