ed3944cc75fa145c98bf893d832c8b39a6f0e8cc
m-ohammed45 <141959137+m-ohammed45@users.noreply.github.com>
2023-11-23 11:55:51 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,31 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include "main.h"
 /**
- * unsigned int binary_to_uint(const char *b);
- *
- * Return: Always 0.
+ *binary_to_uint-convert abinary number to an unsigned int
+ * @b: char string
+ * Return:converted decimal number or 0
  */
-unsigned int binary_to_uint(const char *b)
+
+unsigned int binary_to_uint(const char *b) {
+        unsigned int akron, power;
+        int bex;
+
+if (b == NULL) {
+        return 0;
+    }
+for (bex = 0; b[bex]; bex++)
 {
-  unsigned int val = 0;
-  int i = 0;
+        if(b [bex]!='0'&& b[bex] != '1')
+                        return (0);
 
-  if (b == NULL)
-    return 0;
+}
 
-  while (b[i] == '0' || b[i] == '1')
-  {  // Found another digit.
-    val <<= 1;
-    val += b[i]-'0';
-    i++;
-  }
-  return val;
+ for(power = 1, akron = 0, bex--; bex >= 0; bex--,power *= 2)
+{
+        if (b[bex] == '1')
+                akron += power;
+}
+
+return (akron);
+
 }
