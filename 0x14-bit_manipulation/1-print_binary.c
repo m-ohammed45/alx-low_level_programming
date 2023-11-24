8705409ad6f6556868050c73f6ebf1b6f6b618d9
root <m-ohammed45>
2023-11-24 08:17:58 -0800

    alx-low_level_programming

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,20 +1,49 @@
 #include <stdio.h>
-void print_binary(unsigned long int n) {
-    int i;    
-    int size = sizeof(unsigned long int) * 8;
+#include "main.h"
 
-    for (i = size - 1; i >= 0; i--) {
-        
-        int bit = (n >> i) & 1;   
-        putchar(bit + '0');
-    }
-    putchar('\n');
+/**
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
+ *
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+	/**
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
 }
 
-int main() {
-    unsigned long int number = 1;
-    printf("Binary representation of %lu: ", number);
-    print_binary(number);
-
-    return 0;
-}
