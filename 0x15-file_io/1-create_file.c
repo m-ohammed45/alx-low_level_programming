3bf3a617842e8c74a83b5980efb5be2a57ad4dcd
root <m-ohammed45>
2023-11-28 22:22:45 -0800

    alx-low_level_programming

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
-        return -1;
-    }
+/**
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: Create a function that creates a file.
+ * Return: 1 on success, -1 on failure
+ */
 
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    
-    if (fd == -1) {
-        return -1; 
-    }
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0, file;
 
-    if (text_content != NULL) {
-        if (write(fd, text_content, strlen(text_content)) == -1) {
-            close(fd);
-            return -1;
-        }
-    }
+	if (filename == NULL)
+		return (-1);
 
-    close(fd);
-    return 1; 
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
 }
-
