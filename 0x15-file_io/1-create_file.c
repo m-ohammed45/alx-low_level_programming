c9d0b4d2a0575cac6058d3e0830410dc6c68e403
root <m-ohammed45>
2023-11-28 22:13:04 -0800

    alx-low_level_programming

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int create_file(const char *filename, char *text_content) {
+    if (filename == NULL) {
+        return -1;
+    }
+
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    
+    if (fd == -1) {
+        return -1; 
+    }
+
+    if (text_content != NULL) {
+        if (write(fd, text_content, strlen(text_content)) == -1) {
+            close(fd);
+            return -1;
+        }
+    }
+
+    close(fd);
+    return 1; 
+}
+
